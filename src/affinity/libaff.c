8fc7a1bdc127c517d03ca87cfeb6b26ac6fea92c
George Stelle <gwstell@sandia.gov>
2015-05-11 09:08:24 -0600

    Formatting fixes

--- src/affinity/libaff.c
+++ src/affinity/libaff.c
@@ -22,18 +22,21 @@ int qt_get_unique_id(int i){
   return i;
 }
 
-void INTERNAL qt_affinity_balanced(int num_workers, int start, hwloc_obj_t obj){
-  if(num_workers > 0){
+void INTERNAL qt_affinity_balanced(int num_workers, 
+                                   int start, 
+                                   hwloc_obj_t obj)
+{
+  if (num_workers > 0){
     hwloc_obj_t child = obj->first_child;
-    if (child){
+    if (child) {
       // Have a child, not a leaf
       int arity = obj->arity;
       int div = num_workers / arity;
       int rem = num_workers % arity;
       int n = start;
-      while(child){
+      while (child) {
         int m = div;
-        if(rem > 0){
+        if (rem > 0) {
           m++;
           rem--;
         }
@@ -45,10 +48,10 @@ void INTERNAL qt_affinity_balanced(int num_workers, int start, hwloc_obj_t obj){
       // No children
       workers.binds[start] = hwloc_bitmap_alloc();
       hwloc_bitmap_copy(workers.binds[start], obj->allowed_cpuset);
-      if(num_workers > 1){
+      if (num_workers > 1) {
         printf("warning: PU oversubscribed\n");
       }
-      if(hwloc_bitmap_weight(obj->allowed_cpuset) != 1){
+      if (hwloc_bitmap_weight(obj->allowed_cpuset) != 1) {
         printf("error: expected pu, got weight %d\n", hwloc_bitmap_weight(obj->allowed_cpuset));
         exit(-1);
       } 
@@ -56,7 +59,8 @@ void INTERNAL qt_affinity_balanced(int num_workers, int start, hwloc_obj_t obj){
   }
 }
 
-int INTERNAL qt_affinity_compact(int num_workers, hwloc_obj_t obj)
+int INTERNAL qt_affinity_compact(int num_workers, 
+                                 hwloc_obj_t obj)
 {
   int n = num_workers;
   hwloc_obj_t child = obj->first_child;
@@ -70,7 +74,7 @@ int INTERNAL qt_affinity_compact(int num_workers, hwloc_obj_t obj)
     // No children, should be PU
     workers.binds[workers.num - n] = hwloc_bitmap_alloc();
     hwloc_bitmap_copy(workers.binds[workers.num - n], obj->allowed_cpuset);
-    if(hwloc_bitmap_weight(obj->allowed_cpuset) != 1){
+    if (hwloc_bitmap_weight(obj->allowed_cpuset) != 1){
       printf("error: expected pu, got weight %d\n", hwloc_bitmap_weight(obj->allowed_cpuset));
       exit(-1);
     } 
@@ -90,19 +94,19 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
   hwloc_topology_init(&topology);
   hwloc_topology_load(topology);
   const char *bindstr = qt_internal_get_env_str("CPUBIND", "NOT_SET");
-  if(!bindstr || strcmp("NOT_SET", bindstr) == 0){
+  if (!bindstr || strcmp("NOT_SET", bindstr) == 0) {
     size_t num_sheps = qt_internal_get_env_num("NUM_SHEPHERDS", 1, 0);
     size_t num_workers_per_shepherd = qt_internal_get_env_num("NUM_WORKERS_PER_SHEPHERD", 0, 0);
     size_t num_workers = qt_internal_get_env_num("HWPAR", 0, 0);
     const char * affinity = qt_internal_get_env_str("LAYOUT", "NOT_SET");
     hwloc_const_bitmap_t allowed = hwloc_bitmap_dup(hwloc_topology_get_allowed_cpuset(topology));
-    if(!allowed){
+    if (!allowed) {
       printf("hwloc detection of allowed cpus failed\n");
       exit(-1);
     }
 
     // Determine number of workers
-    if(num_workers){
+    if (num_workers) {
       workers.num = num_workers;
     } else if (num_workers_per_shepherd && num_sheps) {
       workers.num = num_workers_per_shepherd * num_sheps;
@@ -111,22 +115,22 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
     } 
 
     // Determine number of shepherds 
-    if(num_sheps){
+    if (num_sheps) {
       sheps.num = num_sheps;
     } else {
       sheps.num = 1;
     }
 
     workers.binds = malloc(sizeof(hwloc_cpuset_t) * workers.num);
-    for(int i = 0; i< workers.num; i++){
+    for (int i = 0; i< workers.num; i++) {
       workers.binds[i] = hwloc_bitmap_alloc();  
       workers.binds[i] = hwloc_bitmap_dup(allowed);
     }
     hwloc_obj_t obj = hwloc_get_obj_inside_cpuset_by_depth(topology, allowed, 0, 0);
-    if (affinity && strcmp("COMPACT", affinity) == 0){
+    if (affinity && strcmp("COMPACT", affinity) == 0) {
       qt_affinity_compact(workers.num, obj);
     }
-    if (affinity && strcmp("BALANCED", affinity) == 0){
+    if (affinity && strcmp("BALANCED", affinity) == 0) {
       qt_affinity_balanced(workers.num, 0, obj);
     }
 
@@ -139,15 +143,17 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
     int i,j;
     sheps.num = 1; 
     i = 0;
-    while(bstr[i] != 0){
-      if(bstr[i] == ':') sheps.num ++;
+    while (bstr[i] != 0) {
+      if (bstr[i] == ':') {
+        sheps.num ++;
+      }
       i++;
     }
     char ** ranges = malloc(sizeof(char**) * sheps.num);
     j = 0;
     ranges[j++] = bstr; 
-    for(i=0; bstr[i] != 0; i++){
-      if(bstr[i] == ':'){
+    for (i=0; bstr[i] != 0; i++) {
+      if (bstr[i] == ':') {
         bstr[i] = 0;
         ranges[j++] = bstr+i+1;
       }
@@ -155,7 +161,7 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
 
     sheps.binds = malloc(sizeof(hwloc_cpuset_t) * sheps.num);
     workers.num = 0;
-    for(i=0; i<sheps.num; i++){
+    for (i=0; i<sheps.num; i++) {
       char tmp[256];
       sheps.binds[i] = hwloc_bitmap_alloc();
       hwloc_bitmap_list_sscanf(sheps.binds[i], ranges[i]);
@@ -165,7 +171,7 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
     j = 0;
     int id;
     workers.binds = malloc(sizeof(hwloc_cpuset_t) * workers.num);
-    for(i=0; i<sheps.num; i++){
+    for (i=0; i<sheps.num; i++) {
       char tmp[256];
       hwloc_bitmap_foreach_begin(id, sheps.binds[i])
         workers.binds[j] = hwloc_bitmap_alloc();
@@ -180,7 +186,7 @@ void INTERNAL qt_affinity_init(qthread_shepherd_id_t *nbshepherds,
   *hw_par = workers.num; 
 
   // Sanity check
-  if(workers.num % sheps.num != 0){
+  if (workers.num % sheps.num != 0) {
     printf("error: non-uniform number of workers per shepherd\n");
     exit(-1);
   }
@@ -205,7 +211,6 @@ int INTERNAL qt_affinity_gendists(qthread_shepherd_t   *sheps,
     for (size_t j = 0, k = 0; j < nshepherds; ++j) {
       if (j != i) {
         sheps[i].shep_dists[j] = 10;
-        //qthread_debug(AFFINITY_DETAILS, "pretending distance from %i to %i is %i\n", (int)i, (int)j, (int)(sheps[i].shep_dists[j]));
         sheps[i].sorted_sheplist[k++] = j;
       }
     }
